ca23999e36f2c04726280a2f9acf4e127f754fb3
lolottetheclash <35332974+lolottetheclash@users.noreply.github.com>
2019-01-22 12:37:06 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,5 +4,8 @@ Bundler.require
 
 $:.unshift File.expand_path("./../lib", __FILE__)
 
+
 require 'app/scrapper'
+page_mairie = Scrapper.new("http://annuaire-des-mairies.com/val-d-oise.html")
+puts page_mairie.mairies
 
